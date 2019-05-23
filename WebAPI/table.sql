0cd50ee74acac43522ea8ee248f8c9846409ecba
LucasRKrueger <lucasrkthd@gmail.com>
2019-05-23 17:57:59 -0300

    Creating the project

---
+++ WebAPI/table.sql
@@ -0,0 +1,8 @@
+﻿DROP TABLE Employer;
+CREATE TABLE Employer(
+EmployeeID int identity(1,1) primary key,
+FullName varchar(70) NOT NULL,
+EMPCode varchar (20)NOT NULL,
+Mobile varchar(50) NOT NULL,
+Position varchar(50) NOT NULL
+);
