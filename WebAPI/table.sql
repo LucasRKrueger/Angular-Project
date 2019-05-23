0b3eaf24444918e776f73286f28cab7bc8f25151
LucasRKrueger <lucasrkthd@gmail.com>
2019-05-23 18:15:02 -0300

    DB and Entity Framework

--- WebAPI/table.sql
+++ WebAPI/table.sql
@@ -1,8 +1,9 @@
-﻿DROP TABLE Employer;
-CREATE TABLE Employer(
-EmployeeID int identity(1,1) primary key,
-FullName varchar(70) NOT NULL,
-EMPCode varchar (20)NOT NULL,
-Mobile varchar(50) NOT NULL,
-Position varchar(50) NOT NULL
+﻿CREATE TABLE Employer
+(
+	EmployeeID INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+	FullName VARCHAR(70) NULL,
+	EMPCode VARCHAR(20) NULL,
+	Mobile VARCHAR(50) NULL,
+	Position VARCHAR(50) NULL
+
 );
